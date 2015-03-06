d151099a67f2794190cd8438ee0291c4d039ad4a
danielaotelea <daniela.otelea@gmail.com>
2015-03-06 21:45:00 +0200

    implemented tasks for laboratory 02

--- ActivityLifecycleMonitor/src/ro/pub/cs/systems/pdsd/lab02/graphicuserinterface/LifecycleMonitorActivity.java
+++ ActivityLifecycleMonitor/src/ro/pub/cs/systems/pdsd/lab02/graphicuserinterface/LifecycleMonitorActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 
@@ -67,14 +68,23 @@ public class LifecycleMonitorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	
+    	if(savedInstanceState != null)
+    	{
+    		Log.d(Constants.TAG, "onCreate() ==> aplicatia a fost deja lansata");
+    	} else
+    	{
+    		Log.d(Constants.TAG, "onCreate() => prima lansare a aplicatiei");
+    	}
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle_monitor);
-        
+       
         Button okButton = (Button)findViewById(R.id.ok_button);
         okButton.setOnClickListener(buttonClickListener);
         Button cancelButton = (Button)findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(buttonClickListener);
-        Log.d(Constants.TAG, "onCreate() method was invoked");
+        
     }    
 
     @Override
@@ -94,5 +104,80 @@ public class LifecycleMonitorActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState)
+    {
+    	super.onSaveInstanceState(savedInstanceState);
+    	CheckBox checkBox = (CheckBox)findViewById(R.id.remember_me_checkbox);
+    	if(checkBox.isChecked())
+    	{
+    		EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+			EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+		
+			savedInstanceState.putBoolean(Constants.REMEMBER_ME_CHECKBOX, checkBox.isChecked());
+			savedInstanceState.putString(Constants.USERNAME_EDIT_TEXT,usernameEditText.getText().toString());
+			savedInstanceState.putString(Constants.PASSWORD_EDIT_TEXT,passwordEditText.getText().toString());
+    	}
+    }
+    
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+    	super.onRestoreInstanceState(savedInstanceState);
+		if(savedInstanceState.containsKey(Constants.REMEMBER_ME_CHECKBOX)) 
+		{
+			CheckBox checkBox = (CheckBox)findViewById(R.id.remember_me_checkbox);
+			checkBox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_ME_CHECKBOX));
+		}
+		
+		if(savedInstanceState.containsKey(Constants.USERNAME_EDIT_TEXT)) 
+		{
+			EditText usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+			usernameEditText.setText(savedInstanceState.getString(Constants.USERNAME_EDIT_TEXT));
+		}
+		
+		if(savedInstanceState.containsKey(Constants.PASSWORD_EDIT_TEXT)) 
+		{
+			EditText passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+			passwordEditText.setText(savedInstanceState.getString(Constants.PASSWORD_EDIT_TEXT));
+		}
+	}
+    
+    @Override
+    protected void onStart() {
+      super.onStart();
+      Log.d(Constants.TAG, "onStart() a fost apelata!");
+    }
+   
+    @Override
+    protected void onResume() {
+      super.onResume();
+      Log.d(Constants.TAG, "onResume() a fost apelata!");
+    }
+   
+    @Override
+    protected void onPause() {
+      super.onPause();
+      Log.d(Constants.TAG, "onPause() a fost apelata!");
+    }
+   
+    @Override
+    protected void onStop() {
+      super.onStop();
+      Log.d(Constants.TAG, "onStop() a fost apelata!");
+    }
+   
+    @Override
+    protected void onDestroy() {
+      super.onDestroy();
+      Log.d(Constants.TAG, "onDestroy( a fost apelata!");
+    }
+   
+    @Override
+    protected void onRestart() {
+      super.onRestart();
+      Log.d(Constants.TAG, "onRestart() a fost apelata!!");
     }
 }
